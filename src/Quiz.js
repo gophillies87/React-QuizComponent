194703c569d22c1926e8dc1f7ec3d89c29baf345
gophillies87 <44510221+gophillies87@users.noreply.github.com>
2018-12-01 15:24:40 -0500

    Pusing

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,9 @@ class Quiz extends Component {
 }
   render() {
     return (
-      <QuizQuestion />
+	  <div
+         <QuizQuestion quiz_question={quizData.quiz_question[this.state.quiz_position - 1]}/>
+      </div>
     )
   }
 }
