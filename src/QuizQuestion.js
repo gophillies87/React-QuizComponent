17e2797a58d533242414279b45be84cb8ff1583d
gophillies87 <44510221+gophillies87@users.noreply.github.com>
2018-12-01 16:21:20 -0500

    Push

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,10 +4,17 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 let quizData = require('./quiz_data.json')
 
 class QuizQuestion extends Component {
+  constructor(props) {
+	  super(props)
+	  this.state = {incorrectAnswer: false }
+  }
   handleClick(buttonText) {
 	if (buttonText === this.props.quiz_question.answer) {
+		this.setState({ incorrectAnswer: false })
 		this.props.showNextQuestionHandler()
-    }
+    } else {
+		this.setState({ incorrectAnswer: true })
+	}
   }
   render() {
     return (
@@ -24,6 +31,7 @@ class QuizQuestion extends Component {
 
 	        </ul>
 	      </section>
+	      {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
       </main>
     )
   }
