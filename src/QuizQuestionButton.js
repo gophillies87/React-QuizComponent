d5f4b41d4ab464307cb21b8865dc35b0f44ed918
gophillies87 <44510221+gophillies87@users.noreply.github.com>
2018-12-01 15:32:48 -0500

    Create QuizQuestionButton.js

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class QuizQuestionButton extends Component {
+  render() {
+    return (
+       <div />
+    )
+  }
+}
+
+export default QuizQuestionButton
